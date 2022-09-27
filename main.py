230aea0f91648be2873c8dba27f3b471025d1d1d
Hedgehawk11 <84346970+Hedgehawk11@users.noreply.github.com>
2022-09-27 18:39:45 +0000

    this is a commit

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pygame
 import sys as sus
 import random as r
 import RUN
+import time as t
 #import unfair
 #import noncence
 
+pygame.init()
 money = 10
 enters = 0
 loop = True
@@ -27,3 +29,24 @@ while True:
     break
 
 RUN.funsies()
+
+while loop:
+  if money == 0:
+    sus.stop('NO MONEY')
+  input('\nGIVE ME YOUR MONEY \n' + 'you have ' + str(money) + ' left\n')
+  money -= 1
+  if r.randint(0,3) == 2:
+    loop = False
+
+print('here\'s no money')
+money -= 2
+if money <=0:
+    sus.quit('NO MONEY')
+t.sleep(5)
+print('and taxes')
+
+money -= 2
+if money <=0:
+    sus.quit('NO MONEY')
+
+pygame.display.set_mode([400,300])
